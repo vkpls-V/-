14b8f35e09833352697ebd8b582388155bcfb5e7
vika.plisova@outlook.com <vika.plisova@outlook.com>
2022-02-14 10:57:17 +0300

    Задача 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace MySumm
+{
+    class MySumm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mysum");
+            Console.WriteLine(args.Length);
+            Console.WriteLine(Convert.ToInt32(args[0]) + Convert.ToInt32(args[1]));
+        }
+    }
+}
