e01c9c6b4eb2dcd90eb0e8dff575452cdd860e9e
4821057 <4821057@LAPTOP-KPITAPBI>
2022-02-14 09:59:54 +0300

    Добавьте файлы проекта.

---
+++ Задачи/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Задачи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
